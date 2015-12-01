83f2e0e5e14fec07f65753f6dc65521eb68d5d1d
Paula Pinheiro <aluap.pinheiro@gmail.com>
2015-12-01 01:07:21 -0200

    add films correction

--- client/init.js
+++ client/init.js
@@ -1,6 +1,6 @@
 Meteor.startup(function() {
   Uploader.finished = function(index, fileInfo, body) {
-    if (fileInfo.subDirectory == 'file') {
+    if (fileInfo.subDirectory == 'files') {
       Session.set("press_kit_path", fileInfo.path);
     } else {
       Session.set("poster_path", fileInfo.path);
