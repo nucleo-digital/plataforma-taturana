c8938abbcf9898a893e60b6a1de525e9fdd1697d
MATEUS BRUM <mateushenriquebrum@gmail.com>
2015-12-10 11:44:52 -0200

    rules user admin

--- server/init.js
+++ server/init.js
@@ -10,6 +10,23 @@ Meteor.methods({
   }
 });
 Meteor.startup(function () {
+
+  var name = "admin"
+  var email = "admin@plataforma.taturana.com.br"
+  var password = "12345678";
+
+  if(Meteor.users.find({'emails.address':email}).count()===0){
+      console.log("Creating user admin");
+      Accounts.createUser({
+        email: email,
+        password: password,
+        profile : {
+          roles:['admin'],
+          name: name
+        }
+      });
+  }
+
   UploadServer.init({
     tmpDir: process.env.PWD + '/uploads/tmp',
     uploadDir: process.env.PWD + '/uploads/',
